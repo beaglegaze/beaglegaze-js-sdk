39e2b6ee4d90a39cfebb5d7136d7fd5c93ea3b27
steffenboe <steffenb1991.8@gmail.com>
2025-11-04 21:14:54 +0000

    test ci

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
 
 module.exports = {
   input: 'src/index.js',
@@ -15,8 +16,47 @@ module.exports = {
     }
   ],
   plugins: [
-    resolve(),
-    commonjs()
+    json(), // Add JSON plugin to handle JSON imports
+    resolve({
+      preferBuiltins: true, // Prefer Node.js built-ins
+      browser: false // We're building for Node.js
+    }),
+    commonjs({
+      include: /node_modules/ // Only transform CommonJS modules in node_modules
+    })
   ],
-  external: [] // Add external dependencies here if needed
+  external: [
+    // Mark Node.js built-ins and peer dependencies as external
+    'ethers',
+    'fs', 
+    'path',
+    'crypto',
+    'util',
+    'events',
+    'stream',
+    'os',
+    'url',
+    'querystring',
+    'assert',
+    'buffer',
+    'child_process',
+    'cluster',
+    'dgram',
+    'dns',
+    'domain',
+    'http',
+    'https',
+    'net',
+    'punycode',
+    'readline',
+    'repl',
+    'string_decoder',
+    'sys',
+    'timers',
+    'tls',
+    'tty',
+    'v8',
+    'vm',
+    'zlib'
+  ]
 };
